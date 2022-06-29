b0d4c95dae821e326ac276c2669ff2855813803d
dinimm45 <73088172+dinimm45@users.noreply.github.com>
2022-06-29 13:23:45 +0200

    Update ImgDetect.js

--- ImgDetect.js
+++ ImgDetect.js
@@ -138,7 +138,7 @@ video.setAttribute('playsinline', '');
                 
                 console.log(prediction[1].className +  " " + prediction[1].probability.toFixed(2));
                 
-                if (recycled >= 0.20){
+                if (recycled >= 0.70){
                     alert('droped')
                     return;
                   }
